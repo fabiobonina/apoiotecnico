3cf8f585d4bb11a0ab0f9d23c65e3cdfab5982be
Fabio Bonina <fabiobonina@gmail.com>
2016-12-28 07:12:54 -0300

    beta6

--- index.1.php
+++ index.1.php
@@ -27,23 +27,7 @@
     <?php echo $latitude; ?> <?php echo $longitude; ?><?php echo $localidade; ?> <?php echo $cont_oatTt; ?>
     <?php }
     }endforeach; ?>
-<html>
-  <head>
-    <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-  <script src="http://maps.google.com/maps/api/js?key=AIzaSyD690bEo7B-V4nQR5T8-aiyf61bbGzrL6Q" type="text/javascript"></script>
-  
-    <script type='text/javascript'>
-     google.charts.load('upcoming', {'packages': ['geochart', 'table']});
-     google.charts.setOnLoadCallback(drawMarkersMap);
 
-      function drawMarkersMap() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('number', 'Latitude');
-        data.addColumn('number', 'Longitude');
-        data.addColumn('string', 'Label');
-        data.addColumn('number', 'Value 1');
-        data.addRows([
          <?php foreach($localidades->findAll() as $key => $value):if($value->ativo == 0 ) {
             $localId = $value->id;
             $localidade = $value->cliente . " | " . $value->nome;
@@ -53,25 +37,31 @@
             foreach($oats->findAll() as $key => $value):if($value->ativo == 0 && $value->localidade == $localId  ) {
               $cont_oatTt++;
             }endforeach;
-            if( $cont_oatTt > 0 && $latitude <> 0){ ?>
-          [<?php echo $latitude; ?>, <?php echo $longitude; ?>, '<?php echo $localidade; ?>', <?php echo $cont_oatTt; ?>],
-          <?php } 
-          }endforeach; ?>
-        ]);
+            if( $cont_oatTt > 0 && $latitude <> 0){
+              //[ echo $latitude; ,  echo $longitude; , echo $localidade;',  echo $cont_oatTt; ],
+              // Criamos um Array com algumas informações básicas
+              // de uma pessoa.
+              $localOS_info = array(
+                  $localidade {
+                    'center {
+                      lat =>'. $latitude,
+                      lng= $longitude
+                      },
+                    population: $cont_oatTt
+                     },
+                  "idade" => 22,
+                  "profissao" => "Webmaster",
+                  "cidade" => utf8_encode("São Paulo"),
+                  "interesses" => array(utf8_encode("informática"), utf8_encode("programação"), "esportes")
+              );
+            //chicago: { center: {lat: 41.878, lng: -87.629}, population: 2714856},
+              // Agora transformamos esse Array em uma String
+              // formatada em JSON
+              $json = json_encode($localOS_info);
 
-        var options = {
-          region: 'BR',
-          displayMode: 'markers',
-          colorAxis: {colors: ['green', 'blue']}
-        };
+              echo $json;
 
+            }
+          }endforeach;
 
-          var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
-          chart.draw(data, options);
-        };
-    </script>
-  </head>
-  <body>
-    <div id="chart_div" style="width: 900px; height: 500px;"></div>
-  </body>
-</html>
+  ?>
