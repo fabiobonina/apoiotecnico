12d3e73c2bffd5c277ad0a4018d170b2b8d1212a
fabiobonina <fabiobonina@gmail.com>
2017-03-01 12:18:40 -0300

    maps

--- index.1.php
+++ index.1.php
@@ -32,65 +32,38 @@
     <?php }
     }endforeach; ?>
 
-         <?php foreach($localidades->findAll() as $key => $value):if($value->ativo == 0 ) {
-            $localId = $value->id;
-            $localidade = $value->cliente . " | " . $value->nome;
-            $latitude = $value->latitude;
-            $longitude = $value->longitude;
-            $cont_oatTt = 0;
-            foreach($oats->findAll() as $key => $value):if($value->ativo == 0 && $value->localidade == $localId  ) {
-              $cont_oatTt++;
+   <?php
+    foreach($localidades->findAll() as $key => $value):if($value->ativo == 0 ) {
+      $localId = $value->id;
+      $localidade = $value->cliente . " | " . $value->nome;
+      $latitude = $value->latitude;
+      $longitude = $value->longitude;
+      $cont_oatTt = 0;
+      foreach($oats->findAll() as $key => $value):if($value->ativo == 0 && $value->localidade == $localId  ) {
+        $cont_oatTt++;
+      }endforeach;
+      if( $cont_oatTt > 0 && $latitude <> 0){
+        //[ echo $latitude; ,  echo $longitude; , echo $localidade;',  echo $cont_oatTt; ],
+        // Criamos um Array com algumas informações básicas
+        // de uma pessoa.
+      }
+    }endforeach;
+   ?>
+
+
+  			<?php foreach($oats->ultimaOat() as $key => $value):{
+            $oatLocalId = $value->localidade;
+            $oatServId = $value->servico;
+            $oatSistId = $value->sistema;
+            $oatUltimoAtend = $value->UltimaData;
+            foreach($localidades->findAll() as $key => $value):if($value->id == $oatLocalId) {
+              $oatLocal = $value->nome;
             }endforeach;
-            if( $cont_oatTt > 0 && $latitude <> 0){
-              //[ echo $latitude; ,  echo $longitude; , echo $localidade;',  echo $cont_oatTt; ],
-              // Criamos um Array com algumas informações básicas
-              // de uma pessoa.
-            }
-         }endforeach;
+            foreach($servicos->findAll() as $key => $value):if($value->id == $oatServId) {
+              $oatServico = $value->descricao;
+            }endforeach;
+            foreach($sistemas->findAll() as $key => $value):if($value->id == $oatSistId) {
+              $oatSistema =  $value->descricao;
+            }endforeach;
 
-
-  ?>
-
-                    <table id="datatable-buttons" class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>#</th>
-                          <th>Localidades</th>
-                          <th>Servicos</th>
-                          <th>Sistemas</th>
-                          <th>Data</th>
-                          
-                        </tr>
-                      </thead>
-
-                			<?php foreach($oats->ultimaOat() as $key => $value):{ 
-                          $oatLocalId = $value->localidade;
-                          $oatServId = $value->servico;
-                          $oatSistId = $value->sistema;
-                          $oatUltimoAtend = $value->UltimaData;
-                          foreach($localidades->findAll() as $key => $value):if($value->id == $oatLocalId) {
-                            $oatLocal = $value->nome;
-                          }endforeach;             
-                          foreach($servicos->findAll() as $key => $value):if($value->id == $oatServId) {
-                            $oatServico = $value->descricao;
-                          }endforeach;
-                          foreach($sistemas->findAll() as $key => $value):if($value->id == $oatSistId) {
-                            $oatSistema =  $value->descricao;
-                          }endforeach;
-                          
-                      ?>
-                      <tbody>
-                        <tr>
-                          <td><?php echo $oatLocalId; ?></td>
-                          <td><?php echo $oatLocal; ?></td>
-                          <td><?php echo $oatServico; ?></td>
-                          <td><?php echo $oatSistema; ?></td>
-                          <td><?php echo $oatUltimoAtend; ?></td>
-
-                          
-                        </tr>
-                      </tbody>
-                      <?php }endforeach; ?>
-
-                    </table>
-
+        ?>
