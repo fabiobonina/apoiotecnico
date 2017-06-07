db740c13bd8cbf78e6e67c0bf7882944ccd150d9
fabiobonina <fabiobonina@gmail.com>
2017-06-07 07:42:08 -0300

    teste

--- api/supervisorio.php
+++ api/supervisorio.php
@@ -1,37 +1,45 @@
 <?php
+$dns = "mysql:host=localhost;dbname=test";
+$user = "root";
+$pass = "";
+
+/*
+
 
 $dns = "mysql:host=mysql.hostinger.com.br;dbname=u634432767_tec";
 $user = "u634432767_tec";
 $pass = "lYhdZ58UEU";
-/*
-$dns = "mysql:host=localhost;dbname=test";
-$user = "root";
-$pass = "";
 */
 //$_GET, $_POST e $_COOKIE.
 //RECUPERAÇÃO DO FORMULÁRIO
-if(isset($_GET['cmd'])){
-    $data = $_GET['cmd'];
 
-    echo $data;
-    //var_dump ($data);
-    $objData = $data;
 
-    try {
-        $con = new PDO($dns, $user, $pass);
+    //$objData = $_POST['cmd'];//'17/05/2017 16:37:00;9;ABS CEL/ETH IO (4ED/8EA/4SD);EC_1;Entrada de Contagem 1;294.000;EC_2;121.000';
 
-        if(!$con){ echo "Não foi possivel conectar com Banco de Dados!"; };
-        $sql  = "INSERT INTO tb_teste (bem) ";
-        $sql .= "VALUES (:bem)";
-        $query = $con->prepare($sql);
-        $query->bindParam(':bem',$objData);
-        return $query->execute();
+    //echo $objData;
+    //var_dump ($objData);
+    /*
+    $array=explode(";",$objData); 
+           
+    $data = $array[0];
+    $cod1 =$array[1];
+    $equip1 =$array[2];
+    $tipo1 =$array[3];
+    $val1 =$array[4];
+    $equip2 =$array[4];
+    $tipo2 =$array[5];
 
-        echo "OK";
-    } catch (Exception $e) {
-        echo "Erro: ". $e->getMessage();
-    };
-}
+    foreach ($array as $key => $value) {
+            if($value <> ""){
+            echo "Key: $key; Value: $value<br />\n";
+            $value{strlen($value)-1};
+            $value = $value;
+            }
+        }
+
+							
+    */
+
 
 if(isset($_POST['cmd'])){
     $data = $_POST['cmd'];
@@ -44,55 +52,6 @@ if(isset($_POST['cmd'])){
         $con = new PDO($dns, $user, $pass);
 
         if(!$con){ echo "Não foi possivel conectar com Banco de Dados!"; };
-        $sql  = "INSERT INTO tb_teste (bem) ";
-        $sql .= "VALUES (:bem)";
-        $query = $con->prepare($sql);
-        $query->bindParam(':bem',$objData);
-        return $query->execute();
-
-        echo "OK";
-    } catch (Exception $e) {
-        echo "Erro: ". $e->getMessage();
-    };
-}
-
-if(isset($_COOKIE['cmd'])){
-    $data = $_POST['cmd'];
-
-    echo $data;
-    //var_dump ($data);
-    $objData = $data;
-
-    try {
-        $con = new PDO($dns, $user, $pass);
-
-        if(!$con){ echo "Não foi possivel conectar com Banco de Dados!";         };
-        $sql  = "INSERT INTO tb_teste (bem) ";
-        $sql .= "VALUES (:bem)";
-        $query = $con->prepare($sql);
-        $query->bindParam(':bem',$objData);
-        return $query->execute();
-        echo "OK";
-    } catch (Exception $e) {
-        echo "Erro: ". $e->getMessage();
-    };
-}
-
-if(isset($_REQUEST['cmd'])){
-    $data = $_POST['cmd'];
-
-    echo $data;
-    //var_dump ($data);
-    $objData = $data;
-
-
-
-    try {
-        $con = new PDO($dns, $user, $pass);
-
-        if(!$con){
-            echo "Não foi possivel conectar com Banco de Dados!";
-        };
         $sql  = "INSERT INTO tb_teste (bem) ";
         $sql .= "VALUES (:bem)";
         $query = $con->prepare($sql);
